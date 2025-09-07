6c43f91a660512e503700ca3eec7761c9092815a
EugeneSolenov <solenov2007@gmail.com>
2025-09-07 13:01:03 +0300

    Add files via upload

---
+++ Практика №1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+double memory = 0;
+while (true)
+{
+    Console.WriteLine("Первое число введи: ");
+    double first = double.Parse(Console.ReadLine() ?? "");
+    Console.WriteLine("Операцию тоже введи: ");
+    string op = Console.ReadLine() ?? "";
+    switch (op)
+    {
+        case ("+"):
+            Console.WriteLine("Второе число введи: ");
+            double second = double.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine($"{first + second}");
+            break;
+        case ("-"):
+            Console.WriteLine("Второе число введи: ");
+            second = double.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine($"{first - second}");
+            break;
+        case ("*"):
+            Console.WriteLine("Второе число введи: ");
+            second = double.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine($"{first * second}");
+            break;
+        case ("/"):
+            Console.WriteLine("Второе число введи: ");
+            second = double.Parse(Console.ReadLine() ?? "");
+            if (second == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+                break;
+            }
+            Console.WriteLine($"{first / second}");
+            break;
+        case ("%"):
+            Console.WriteLine("Второе число введи: ");
+            second = double.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine($"{first % second}");
+            break;
+        case ("1/x"):
+            if (first == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+                break;
+            }
+            Console.WriteLine($"{1/first}");
+            break;
+        case ("x^2"):
+            Console.WriteLine($"{first*first}");
+            break;
+        case ("sqrt"):
+            Console.WriteLine($"{Math.Sqrt(first)}");
+            break;
+        case ("M+"):
+            memory += first;
+            Console.WriteLine($"Память обновлена");
+            break;
+        case ("M-"):
+            memory -= first;
+            Console.WriteLine($"Память обновлена");
+            break;
+        case ("MR"):
+            Console.WriteLine($"{memory}");
+            break;
+        case ("esc"):
+            return;
+
+    }
+}
+/*Фунĸционал:
+ Простой ĸальĸулятор, позволяет пользователю вписать первое значение, далее выбрать 
+нужную ему операцию, и в зависимость от операции, ему может понадобиться добавить 
+второе значение, либо же сразу получить ответ на свой запрос.
+ Ограничения:
+ ĸальĸулятор не позволяет делить на ноль, предупреждая пользователя о невозможности 
+таĸой операции. Таĸ же диапазон вычислений ограничен ограничениями типа “double” в c#, 
+то есть: от +- 5.0^10-324 до +- 1.7^10308. А таĸже +-15 знаĸов после запятой.
+ Возмозжные ошибĸи:
+При попытĸе вписать вместо чисел любые другие символы произойдет ошибĸа, ĸод 
+расчитан на то, что пользователь будет ĸорреĸтно вводить данные.*/
+
